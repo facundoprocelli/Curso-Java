e76c498c706742e039fc862f59d31411b0da5890
Facundo Procelli <facundoprocelli@gmail.com>
2024-03-04 19:19:35 -0300

    agregamso los tipos de datos

--- HolaMundo/Hola.java
+++
@@ -1,8 +0,0 @@
-public class Hola {
-    
-    public static void main(String args[]){
-
-        System.out.println("este es un segundo archivo ");
- 
-     }
-}
---
+++ HolaMundo/TiposDeDatos.java
@@ -0,0 +1,13 @@
+public class TiposDeDatos {
+    
+    public static void main(String args[]){
+
+        int numero = 5;
+        String nombre = "facundo";
+        char letra = 'f';
+        double temperatura = 35.4;
+        boolean siONo = false;
+        long largo = 123456789;
+        
+     }
+}
