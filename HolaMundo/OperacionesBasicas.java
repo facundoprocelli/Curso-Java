be0464b20cb81a10bf3791b8e63cf5dca0ecc156
Facundo Procelli <facundoprocelli@gmail.com>
2024-03-04 19:35:01 -0300

    Operaciones basicas

---
+++ HolaMundo/OperacionesBasicas.java
@@ -0,0 +1,26 @@
+public class OperacionesBasicas {
+
+    
+	public static void main(String args[]){
+
+        int num1, num2;
+        float resultado;
+
+        num1 = 4;
+        num2 = 3;
+
+        resultado = num1 + num2;
+        System.out.println("El resultado de la suma es de: " + resultado);
+        
+        resultado = num1 - num2;
+        System.out.println("El resutlado de la resta es: "+ resultado );
+        
+        resultado = num1 * num2;
+        System.out.println("El resutlado de la multiplicacion es: "+ resultado );
+        
+        resultado = num1 / num2;
+        System.out.println("El resutlado de la Division es: "+ resultado );
+
+    }
+    
+}
