4222a9922cf233a7f7797369875381af6b4b6522
Facundo Procelli <facundoprocelli@gmail.com>
2024-03-05 15:54:40 -0300

    Intercambios de dos numeros

---
+++ HolaMundo/ProblemasBasicos.java
@@ -0,0 +1,15 @@
+public class ProblemasBasicos {
+    
+	public static void main(String args[]){
+
+        int num1 = 35;
+        int num2 = 20;
+
+        System.err.println("Numero 1: " + num1 + " Numero 2: " + num2);
+        int aux = num1;
+        num1 = num2;
+        num2 = aux;
+
+        System.err.println("Numero 1: " + num1 + " Numero 2: " + num2);
+     }
+}
